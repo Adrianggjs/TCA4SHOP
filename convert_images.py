f3f64695d0a57b7a88bbc82ae366fcc3afacbb75
adrianggjs <2007291440101@ingenieria.usac.edu.gt>
2026-08-04 13:04:09 -0600

    Add 5th image to AirPods Pro 3

--- convert_images.py
+++ convert_images.py
@@ -4,8 +4,8 @@ from PIL import Image
 src_dir = r"C:\Users\yolit\.gemini\antigravity\brain\0e6dfb57-e7dd-4936-aec4-bfed9b52c416\.user_uploaded"
 dest_dir = r"C:\Users\yolit\OneDrive\Escritorio\Proyectos\Emprendimiento\PAGINANUEVA\assets\img"
 
-src = "media_1785868859950.jpg"
-out = "airpods_pro3_4.webp"
+src = "media_1785869943206.jpg"
+out = "airpods_pro3_5.webp"
 
 src_path = os.path.join(src_dir, src)
 out_path = os.path.join(dest_dir, out)
